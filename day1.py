a3d81fe250c0737fc600da41097acc67f24c4929
jahnavimanepalli4 <divyamanepalli45@gmail.com>
2026-02-26 15:18:37 +0530

    patterns

---
+++ day1.py
@@ -0,0 +1,99 @@
+# n = int(input("enter the number:"))
+# for i in range(n):
+#     for j in range(n):
+#        print("*",end="")
+#     print()   
+
+
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(i,end = "")
+#     print()    # if columns increment print i
+
+   
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(j,end="")
+#     print()    # if rows increment prinnt j
+
+# count = 0
+# n = int(input("enter the number:"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         count +=1
+#         print(count,end = "")
+#     print()       
+
+# alpha = 65
+# for i in range(n):
+#     for j in range(n):
+#         print(chr(alpha),end="")
+#     print()    
+
+
+# n = int(input("enter the number:"))
+# alpha = 65
+# for i in range(1,n+1):
+#     alpha = 65
+#     for j in range(1,n+1):
+#         print(chr(alpha),end="")
+#         alpha +=1
+#     print() 
+
+# n = int(input("enter the number:"))
+# alpha = 65
+# for i in range(1,n+1):
+#     for j in range(1,n+1):
+#         print(chr(alpha),end="")
+#         alpha +=1
+#     print() 
+# ___________________steps patterns_________________
+n = int(input("enter the number"))
+for i in range(n):
+    for j in range(i):
+        print("*",end="")
+    print()  
+
+
+# alph = 65
+# for i in range(n):
+#     for j in range(i):
+#         print(chr(alph),end="")
+#     print()    
+
+
+# alph = 65
+# for i in range(n):
+#     for j in range(i):
+#         print(chr(alph),end="")
+#         alph +=1
+#     print()   
+
+
+# alph = 65
+# for i in range(n):
+#     alph = 65
+#     for j in range(i):
+#         print(chr(alph),end="")
+#         alph += 1
+#     print()   
+
+
+# for i in range(n):
+#     for j in range(i):
+#         print(i,end="")
+#     print()   
+
+
+# for i in range(n):
+#     for j in range(i):
+#         print(j,end="")
+#     print()   
+
+# count = 0
+# for i in range(1,n):
+#     for j in range(i):
+#         print(count,end="")
+#         count +=1
+#     print()             
+    
